38a79a35754ed259fcc8c219d8568b7c70c9ea25
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-11 20:53:28 +0330

    Feat(component): header section component This sequel has two headers and one file vaginal to adjust each header in the right position. This position depends on the status of users.

---
+++ components/headerComponent/baseHeaderComponent.js
@@ -0,0 +1,69 @@
+import domGenerator from "dom-generator";
+import "./index.scss";
+
+import buttonGenerator from "../buttonComponent/buttonComponent";
+import generateLinks from "../../src/js/creatLink";
+
+/**
+ * Generates a base button element with optional properties.
+ * @param {Object} options - The options for generating the button.
+ * @param {string} options.content - The text content of the button.
+ * @param {string} [options.size="medium"] - The size of the button ("small", "medium", "large").
+ * @param {string} [options.status="primaryFill"] - The status of the button ("primaryFill", "secondaryFill", "tertiaryFill", "quaternaryFill").
+ * @param {string} [options.type="button"] - The type of the button ("button", "submit", "reset").
+ * @param {string} [options.className=""] - Additional class names for the button.
+ * @param {string} [options.anchorLink="#"] - The href link for the button if it acts as an anchor.
+ * @param {Object} [options.eventListeners={}] - Event listeners to attach to the button.
+ * @param {boolean} [options.disabled] - Whether the button should be disabled.
+ * @param {string} [options.iconStart=""] - The URL of the image icon to display at the start of the button.
+ * @param {string} [options.iconEnd=""] - The URL of the image icon to display at the end of the button.
+ * @returns {HTMLDivElement} - The generated button element.
+ */
+
+function headerGenerator({
+  content,
+  size = "medium",
+  statues = "primaryFill",
+  className = "",
+  //   anchorLink = "#",
+  eventListeners = {},
+  logo = "",
+  profile = "",
+  boxBuy = "",
+}) {
+  let header = domGenerator({
+    tag: "div",
+    attributes: {
+      class: `headerWrapper ${className}`,
+    },
+    dataAttributes: { size: size, status: statues },
+
+    children: [
+      {
+        tag: "img",
+        attributes: { src: logo },
+      },
+      {
+        tag: "div",
+        attributes: {
+          class: `headerSection`,
+        },
+        children: [
+          {
+            tag: generateLinks({
+              numberOfAs: 3,
+              texts: ["Link 1", "Link 2", "Link 3"],
+            }),
+          },
+        ],
+      },
+      {
+        tag: buttonGenerator({}),
+      },
+    ],
+  });
+
+  return header;
+}
+
+export default headerGenerator;
