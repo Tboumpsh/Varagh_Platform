911b0eb7994b2fe0ec2d373e753ac9cfb3d78a23
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-16 21:35:26 +0330

    Feat(src): create newest card The function of calling the api and getting the specified information from the database and passing it to the component to display it on the page

---
+++ src/js/createNewestCard.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+import cardGenerator from "../../components/cardComponent/cardComponent";
+
+async function fetchBooksNewest() {
+  try {
+    let { data: books } = await axios.get("http://localhost:3000/books?_limit=3&date_gte=1401");
+
+    // Generate card elements
+    const cardElements = books.map(book => {
+      return cardGenerator({
+        titleContent: book.name,
+        paragraphContent: book.description,
+        size: "medium",
+        statues: "primary",
+        upImage: book.banner,
+      });
+    });
+
+    return cardElements; // Return the array of card elements
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return []; // Return an empty array on error
+  }
+}
+
+export default fetchBooksNewest;
