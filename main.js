996b450c6966a06f882e42810e6d45e6c3285886
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-22 20:25:13 +0330

    Feat(root): main.js This file is not complete and complete, and the jacks are seeing the output for the project leader.

--- main.js
+++ main.js
@@ -1,1 +1,171 @@
-undefined
+// import categorySectionComponent from "sectionComponents/categorySectionComponent/categoryComponentSection";
+import "/styles/cssReset.css";
+import "/styles/index.scss";
+
+import cheapestCardSectionComponent from "./sectionComponent/cheapestCardSectionComponent/CheapestCardSectionComponent";
+import newestCardSectionComponent from "./sectionComponent/newest‌CardSectionComponent/newestCardsectionComponent";
+import bestSellerSectionComponent from "./sectionComponent/bestSellerSectionComponent/bestSellerSectionComponent";
+import categorySectionComponent from "./sectionComponent/categorySectionComponent/categoryComponentSection";
+import aboutSectionComponent from "./sectionComponent/aboutSectionComponent/aboutSectionComponent";
+// import contactUsSection from "./sectionComponent/socialSectionComponent/socialSectionComponent";
+import heroSectionComponent from "./sectionComponent/heroSectionComponent/heroSectionComponent";
+// import aboutSectionComponent from "./components/aboutSectionComponent/aboutSectionComponent";
+import changeProfileSection from "./sectionComponent/changeProfileSection/changeProfileSection";
+import fetchBooksAndRenderTable from "./components/booksTableComponent/booksTableComponent";
+import contactUsComponent from "./sectionComponent/contactUsComponent/contactUsComponent";
+// import fetchUsersAndRenderTable from "./components/UsertableComponent/tableComponent";
+// import fetchUsersAndRenderTable from "./components/UsertableComponent/tableComponent";
+// import contactUsComponent from "./components/contactUsComponent/contactUsComponent";
+import fetchUsersAndRenderTable from "./components/UsertableComponent/tableComponent";
+// import footerSectionComponent from "./components/footerComponent/footerComponent";
+import searchBoxComponent from "./components/searchBoxComponent/searchBoxComponent";
+import changeProfileUserSection from "./pages/changeProfileUser/changeProfileUser";
+// import headerGenerator from "./components/headerComponent/headerComponent";
+import filterComponent from "./components/filterComponent/filterComponent";
+// import tableComponent from "./components/tableComponent/tableComponent";
+import headerGenerator from "./components/headerComponent/headerComponent";
+// import createUserTable from "./sectionComponent/usersTable/usersTable";
+// import cardGenerator from "./components/cardComponent/cardComponent";
+import renderLandingPage from "./pages/landingPage/renderLandingPage";
+// import fetchBooksAndProcess from "./src/js/createBestSellerCard";
+import landingPage from "./pages/landingPage/landingPage";
+import aboutPage from "./pages/aboutUsPage/aboutPage";
+import adminPage from "./pages/adminPage/adminPage";
+import shopPage from "./pages/shopPage/shopPage";
+
+// landingPage()
+// headerGenerator({
+//     content:'header',
+//     size:'medium',
+//     status:'default',
+//     logo: '/public/images/Logo/logo.svg',
+//     boxBuy:'/public/images/header/vector.png'
+// })
+
+// contactUsSection({
+//   titleCategory: "ما را در فضای مجازی دنبال کنید",
+//   size: "small",
+//   status: "default",
+//   instagram: "/public/images/aboutPage/social/insta.png",
+//   linkedin: "/public/images/aboutPage/social/linkdin.png",
+//   whatsapp: "/public/images/aboutPage/social/whatsapp.png",
+//   email: "/public/images/aboutPage/social/email.png",
+// });
+
+// footerSectionComponent({
+//     contentAboutSection:'فروشگاه ورق',
+//     size:'medium',
+//     status:'default',
+//     iconStart:'/public/images/footer/rectangle12.png',
+//     iconEnd:'/public/images/footer/rectangle13.png',
+//     upImage:'/public/images/footer/boy.png'
+// })
+
+// categorySectionComponent({
+//   titleCategory: "دسته بندی",
+//   siz: "medium",
+//   status: "default",
+//   teenager: "/public/images/landinPage/categoryImages/tenager.svg",
+//   old: "/public/images/landinPage/categoryImages/old.svg",
+//   child: "/public/images/landinPage/categoryImages/child.svg",
+// });
+
+// cardGenerator({
+//   titleContent: "کتاب لیلا",
+//   paragraphContent:
+//     "lorem test text contentlorem test text contentlorem test text contentlorem test text contentlorem test text contentlorem test text content",
+//   size: "medium",
+//   status: "primary",
+//   upImage: "/public/images/Logo/logo.svg",
+// });
+
+// aboutSectionComponent({
+//     contentTitle:'درباره ما',
+//     paragraphContent:'در دنیای پرشتاب امروز، همه ما به دنبال یافتن لحظاتی هستیم که بتوانیم به آرامش برسیم و در دنیای کتاب‌ها غوطه‌ور شویم. کتابخانه آنلاین ورق با این هدف راه‌اندازی شده است که دسترسی به دنیایی از دانش، تخیل و الهام را برای شما آسان‌تر کند. ما در ورق بر این باوریم که هر کتاب یک سفر است و هر سفر داستانی دارد که منتظر است تا کشف شود. از داستان‌های هیجان‌انگیز و عاشقانه گرفته تا کتاب‌های علمی و آموزشی، مجموعه‌ای بی‌نظیر از بهترین عناوین را گردآوری کرده‌ایم تا نیازهای مختلف شما را برآورده کنیم.',
+//     size:'medium',
+//     status:'default',
+//     gifSrc:'/public/images/aboutPage/Group-24.gif'
+// })
+
+// aboutSectionComponent({
+//   contentTitle: "درباره ما",
+//   size: "medium",
+//   status: "default",
+// });
+
+// contactUsComponent({
+//   contactTitle:'ارتباط با ما',
+//   mobileTitle:'شماره موبایل',
+//   addressTitle:'آدرس',
+//   telephoneTitle:'شماره تلفن',
+//     size:'medium',
+//     status:'default',
+//     imageSrc:'/public/images/aboutPage/businessidea.png'
+// })
+// heroSectionComponent({
+//   titleHero: "کتابخانه ورق",
+//   contentHero:
+//     'به "کتابخانه ورق" خوش آمدید، جایی که عشق به کتاب و دانش به هم می‌پیوندند. اینجا نه تنها یک کتابخانه، بلکه یک ماجراجویی بی‌پایان در دنیای کتاب‌هاست. با کتابخانه ورق، دنیای جدیدی از اطلاعات و داستان‌ها را کشف کنید و به جمع علاقه‌مندان به کتاب و مطالعه بپیوندیانه ورق" خوش آمدید، جایی که عشق به کتاب و دانش به هم می‌پیوندند. اینجا نه تنها یک کتابخانه، بلکه یک ماجراجویی بی‌پایان در دنیای کتاب‌هاست. با کتابخانه ورق، دنیای جدیدی از اطلاعات و داستان‌ها را کشف کنید و به جمع علاقه‌مندان به کتاب و مطالعه بپیوندید.د.',
+//   size: "medium",
+//   status: "default",
+//   srcHero: "/public/images/landinPage/heroSection/herosection.png",
+// });
+
+// bestSellerSectionComponent({
+//   titleBestSeller:"پر فروش ترین ",
+//   size:'medium',
+//   status:'default',
+
+// })
+
+// newestCardSectionComponent({
+//   titleNewest: "جدیدترین",
+//   size: "medium",
+//   status: "default",
+// });
+
+// cheapestCardSectionComponent({
+//   titleCheapest:'ارزان ترین',
+//   size:'medium',
+//   status:'default'
+// })
+
+// searchBoxComponent({
+//   size: "medium",
+//   status: "default",
+// });
+
+// filterComponent({
+//   size: "medium",
+//   statues: "default",
+// });
+
+// fetchUsersAndRenderTable();
+
+// headerGenerator({
+//   size: "medium",
+//   status: "default",
+//   logo: "/public/images/Logo/logo.svg",
+//   boxBuy: "/public/images/header/vector.png",
+// });
+
+// fetchBooksAndRenderTable()
+
+// changeProfileSection({
+//   changeProfileTitle: "پروفایل من",
+//   changeProfileParagraph: "در اینجا شما می‌توانید تصویری جذاب و منحصر به فرد برای پروفایل خود انتخاب کنید و آن را به راحتی ثبت کنید. ما می‌دانیم که انتخاب یک تصویر مناسب می‌تواند تجربه کاربری شما را شخصی‌تر و دلپذیرتر کند. با چند کلیک ساده، می‌توانید نمایه‌ای ایجاد کنید که نشان‌دهنده شخصیت و علایق شما باشد.",
+//   size: "medium",
+//   status: "default",
+// });
+// shopPage()
+
+// changeProfileUserSection()
+
+
+// adminPage()
+// fetchBooksAndRenderTable()
+// fetchUsersAndRenderTable()
+// aboutPage()
+
+
+renderLandingPage()
