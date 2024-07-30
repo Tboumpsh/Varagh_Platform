ded27f06f741837ca7dc8abebd3b8c0a2b7b64e2
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-30 17:09:30 +0330

    Feat(constants): add feature plan cards Add the price of library Plan Purchase cards. So that they can be easily changed.

--- constants/informationUS.js
+++ constants/informationUS.js
@@ -1,3 +1,7 @@
 export const Telephone = 2154658;
 export const Mobile = 2154658;
-export const Address = 'تهرانـ و برای شرایط فعلی تکنولوژی مورد نیازوع با هاشدی';
+export const Address = "تهرانـ و برای شرایط فعلی تکنولوژی مورد نیازوع با هاشدی";
+
+export const WeeklyPrice = 200000;
+export const yearlyPrice = 75000000;
+export const monthlyPrice = 7000000;
