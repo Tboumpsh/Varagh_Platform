9d1059db0f4542748b4a993a6b8a14231c6e69f1
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:46:53 +0330

    Feat(pages): buy books list On the book viewing page you have a book on the number of books. The price also changes the basis.

--- pages/buyBookListPage/buyBookListPage.js
+++ pages/buyBookListPage/buyBookListPage.js
@@ -4,7 +4,23 @@ import "./index.scss";
 import footerSectionComponent from "../../sectionComponent/footerComponent/footerComponent";
 import headerAdminGenerator from "../../components/headerComponent/adminHeaderComponent";
 import headerGenerator from "../../components/headerComponent/headerComponent";
+import buttonGenerator from "../../components/buttonComponent/buttonComponent";
 import loggedInHeader from "../../components/headerComponent/loggedInHeader";
+import handlePayment from "../../src/js/shaparack/handelPay";
+
+/**
+ * Generates the book list page with appropriate header and footer.
+ * Depending on the role of the current user (admin, logged in, or guest),
+ * it generates different headers.
+ *
+ * This function:
+ * 1. Retrieves the current user's role from local storage.
+ * 2. Generates the appropriate header based on the user's role.
+ * 3. Creates the main content of the book list page including a section for the book list and a footer.
+ *
+ * @function buyBookListPage
+ * @returns {HTMLDivElement} - The generated book list page element.
+ */
 
 function buyBookListPage() {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
@@ -44,6 +60,29 @@ function buyBookListPage() {
       {
         tag: "div",
         attributes: { id: "buyList" },
+      },
+      {
+        tag: "div",
+        attributes: { id: "totalPrice" },
+        children:
+        [
+          {
+            tag: buttonGenerator({
+              content: "پرداخت سبد خرید",
+              size: "medium",
+              status: "primaryOrang",
+              eventListeners: { 
+                click: () => {
+                const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+                  if (currentUser) {
+                    handlePayment(currentUser.name);
+                  } else {
+                    alert("لطفا ابتدا وارد شوید.");
+                  }
+              } },
+            }),
+        },
+        ]
       },
       {
         tag: footerSectionComponent({
