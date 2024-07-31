844e5f028966797834bf27a7c7cafc9a6645a60a
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:37:33 +0330

    Fixed(main): main.js The main project file for displaying the main functioning of the page rendering

--- main.js
+++ main.js
@@ -1,149 +1,25 @@
-// import categorySectionComponent from "sectionComponents/categorySectionComponent/categoryComponentSection";
 import "/styles/cssReset.css";
 import "/styles/index.scss";
 
 import renderLandingPage from "./pages/landingPage/renderLandingPage";
 
-// landingPage()
-// headerGenerator({
-//     content:'header',
-//     size:'medium',
-//     status:'default',
-//     logo: '/public/images/Logo/logo.svg',
-//     boxBuy:'/public/images/header/vector.png'
-// })
+/**
+ * This module imports necessary CSS styles and then renders the landing page.
+ *
+ * - Imports global CSS reset styles from `/styles/cssReset.css`.
+ * - Imports main stylesheet from `/styles/index.scss`.
+ * - Imports the `renderLandingPage` function from `./pages/landingPage/renderLandingPage`.
+ * - Calls the `renderLandingPage` function to initialize and render the landing page.
+ *
+ * This module is responsible for setting up the visual and functional aspects of the landing page by
+ * ensuring that the required styles are applied and the page is rendered properly when the module is executed.
+ *
+ * @module
+ * @requires /styles/cssReset.css
+ * @requires /styles/index.scss
+ * @requires ./pages/landingPage/renderLandingPage
+ */
 
-// contactUsSection({
-//   titleCategory: "ما را در فضای مجازی دنبال کنید",
-//   size: "small",
-//   status: "default",
-//   instagram: "/public/images/aboutPage/social/insta.png",
-//   linkedin: "/public/images/aboutPage/social/linkdin.png",
-//   whatsapp: "/public/images/aboutPage/social/whatsapp.png",
-//   email: "/public/images/aboutPage/social/email.png",
-// });
-
-// footerSectionComponent({
-//     contentAboutSection:'فروشگاه ورق',
-//     size:'medium',
-//     status:'default',
-//     iconStart:'/public/images/footer/rectangle12.png',
-//     iconEnd:'/public/images/footer/rectangle13.png',
-//     upImage:'/public/images/footer/boy.png'
-// })
-
-// categorySectionComponent({
-//   titleCategory: "دسته بندی",
-//   siz: "medium",
-//   status: "default",
-//   teenager: "/public/images/landinPage/categoryImages/tenager.svg",
-//   old: "/public/images/landinPage/categoryImages/old.svg",
-//   child: "/public/images/landinPage/categoryImages/child.svg",
-// });
-
-// cardGenerator({
-//   titleContent: "کتاب لیلا",
-//   paragraphContent:
-//     "lorem test text contentlorem test text contentlorem test text contentlorem test text contentlorem test text contentlorem test text content",
-//   size: "medium",
-//   status: "primary",
-//   upImage: "/public/images/Logo/logo.svg",
-// });
-
-// aboutSectionComponent({
-//     contentTitle:'درباره ما',
-//     paragraphContent:'در دنیای پرشتاب امروز، همه ما به دنبال یافتن لحظاتی هستیم که بتوانیم به آرامش برسیم و در دنیای کتاب‌ها غوطه‌ور شویم. کتابخانه آنلاین ورق با این هدف راه‌اندازی شده است که دسترسی به دنیایی از دانش، تخیل و الهام را برای شما آسان‌تر کند. ما در ورق بر این باوریم که هر کتاب یک سفر است و هر سفر داستانی دارد که منتظر است تا کشف شود. از داستان‌های هیجان‌انگیز و عاشقانه گرفته تا کتاب‌های علمی و آموزشی، مجموعه‌ای بی‌نظیر از بهترین عناوین را گردآوری کرده‌ایم تا نیازهای مختلف شما را برآورده کنیم.',
-//     size:'medium',
-//     status:'default',
-//     gifSrc:'/public/images/aboutPage/Group-24.gif'
-// })
-
-// aboutSectionComponent({
-//   contentTitle: "درباره ما",
-//   size: "medium",
-//   status: "default",
-// });
-
-// contactUsComponent({
-//   contactTitle:'ارتباط با ما',
-//   mobileTitle:'شماره موبایل',
-//   addressTitle:'آدرس',
-//   telephoneTitle:'شماره تلفن',
-//     size:'medium',
-//     status:'default',
-//     imageSrc:'/public/images/aboutPage/businessidea.png'
-// })
-// heroSectionComponent({
-//   titleHero: "کتابخانه ورق",
-//   contentHero:
-//     'به "کتابخانه ورق" خوش آمدید، جایی که عشق به کتاب و دانش به هم می‌پیوندند. اینجا نه تنها یک کتابخانه، بلکه یک ماجراجویی بی‌پایان در دنیای کتاب‌هاست. با کتابخانه ورق، دنیای جدیدی از اطلاعات و داستان‌ها را کشف کنید و به جمع علاقه‌مندان به کتاب و مطالعه بپیوندیانه ورق" خوش آمدید، جایی که عشق به کتاب و دانش به هم می‌پیوندند. اینجا نه تنها یک کتابخانه، بلکه یک ماجراجویی بی‌پایان در دنیای کتاب‌هاست. با کتابخانه ورق، دنیای جدیدی از اطلاعات و داستان‌ها را کشف کنید و به جمع علاقه‌مندان به کتاب و مطالعه بپیوندید.د.',
-//   size: "medium",
-//   status: "default",
-//   srcHero: "/public/images/landinPage/heroSection/herosection.png",
-// });
-
-// bestSellerSectionComponent({
-//   titleBestSeller:"پر فروش ترین ",
-//   size:'medium',
-//   status:'default',
-
-// })
-
-// newestCardSectionComponent({
-//   titleNewest: "جدیدترین",
-//   size: "medium",
-//   status: "default",
-// });
-
-// cheapestCardSectionComponent({
-//   titleCheapest:'ارزان ترین',
-//   size:'medium',
-//   status:'default'
-// })
-
-// searchBoxComponent({
-//   size: "medium",
-//   status: "default",
-// });
-
-// filterComponent({
-//   size: "medium",
-//   statues: "default",
-// });
-
-// fetchUsersAndRenderTable();
-
-// headerGenerator({
-//   size: "medium",
-//   status: "default",
-//   logo: "/public/images/Logo/logo.svg",
-//   boxBuy: "/public/images/header/vector.png",
-// });
-
-// fetchBooksAndRenderTable()
-
-// changeProfileSection({
-//   changeProfileTitle: "پروفایل من",
-//   changeProfileParagraph: "در اینجا شما می‌توانید تصویری جذاب و منحصر به فرد برای پروفایل خود انتخاب کنید و آن را به راحتی ثبت کنید. ما می‌دانیم که انتخاب یک تصویر مناسب می‌تواند تجربه کاربری شما را شخصی‌تر و دلپذیرتر کند. با چند کلیک ساده، می‌توانید نمایه‌ای ایجاد کنید که نشان‌دهنده شخصیت و علایق شما باشد.",
-//   size: "medium",
-//   status: "default",
-// });
-// shopPage()
-
-// changeProfileUserSection()
-
-
-// adminPage()
-// fetchBooksAndRenderTable()
-// fetchUsersAndRenderTable()
-// aboutPage()
-
-
-renderLandingPage()
-// renderAdminPage()
-// renderAboutPage()
-// renderShopPage()
-// renderChangeProfilePage()
-
-// checkUserAndRender()
-// renderChangeProfilePage()
+document.addEventListener("DOMContentLoaded", () => {
+  renderLandingPage();
+});
