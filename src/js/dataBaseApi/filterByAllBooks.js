dfa988ade28d87056715f22c4ea86c61595dce6c
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:32:21 +0330

    docs(src): filter all books Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/js/dataBaseApi/filterByAllBooks.js
+++ src/js/dataBaseApi/filterByAllBooks.js
@@ -2,6 +2,26 @@ import axios from "axios";
 
 import cardGenerator from "../../../components/cardComponent/cardComponent";
 
+/**
+ * Fetches a list of all books from the server and displays them as card elements in the specified container.
+ * 
+ * This asynchronous function performs the following actions:
+ * 1. Sends a GET request to the server to retrieve a list of all books.
+ * 2. Clears the existing content of the element with ID `productsContent`.
+ * 3. Maps the retrieved books to card elements using the `cardGenerator` function.
+ * 4. Appends each card element to the `productsContent` container.
+ * 
+ * If there is an error during the fetch operation, the error is logged to the console.
+ * 
+ * @async
+ * @function filterByAllBooks
+ * @returns {Promise<void>} A promise that resolves when the books have been fetched and displayed.
+ * 
+ * @throws {Error} Throws an error if the GET request fails or if there is an issue with
+ *     processing the response data.
+ * 
+ */
+
 async function filterByAllBooks() {
 
     const productsContent = document.getElementById('productsContent');
