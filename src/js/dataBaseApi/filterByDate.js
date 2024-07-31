f63342658c2eccaec943f98d1b713f389b0b31b5
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:32:34 +0330

    docs(src): filter by date Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/js/dataBaseApi/filterByDate.js
+++ src/js/dataBaseApi/filterByDate.js
@@ -2,6 +2,29 @@ import axios from "axios";
 
 import cardGenerator from "../../../components/cardComponent/cardComponent";
 
+/**
+ * Fetches a list of books from the server and displays them sorted by date.
+ * 
+ * This asynchronous function retrieves books from the server and sorts them based on the provided order.
+ * It then generates card elements for each book and appends them to the element with ID `productsContent`.
+ * 
+ * The sorting order is determined by the `order` parameter:
+ * - If `order` is `'newest'`, the books are sorted in descending order (newest first).
+ * - If `order` is anything else, the books are sorted in ascending order (oldest first).
+ * 
+ * If there is an error during the fetch operation, the error is logged to the console.
+ * 
+ * @async
+ * @function filterByDate
+ * @param {string} order - The order in which to sort the books. Can be `'newest'` or any other value.
+ *     If `'newest'`, books will be sorted by date in descending order. Otherwise, they will be sorted in ascending order.
+ * @returns {Promise<void>} A promise that resolves when the books have been fetched, sorted, and displayed.
+ * 
+ * @throws {Error} Throws an error if the GET request fails or if there is an issue with
+ *     processing the response data.
+ * 
+ */
+
 async function filterByDate(order) {
   let productsContent = document.getElementById('productsContent')
   try {
