8a9d7c3ecea55d68fba12140d4e9e2e2e30b8be6
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:35:23 +0330

    docs(src):logout Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/js/RegisterFormValidation/logOut.js
+++ src/js/RegisterFormValidation/logOut.js
@@ -1,5 +1,21 @@
 import renderLandingPage from "../../../pages/landingPage/renderLandingPage";
 
+/**
+ * Logs out the current user by removing their information from `localStorage` and then
+ * redirects to the landing page.
+ * 
+ * This function performs the following actions:
+ * 1. Removes the `"currentUser"` item from `localStorage`, effectively logging out the user.
+ * 2. Calls `renderLandingPage()` to redirect the user to the landing page or refresh the landing page view.
+ * 
+ * This function does not take any parameters and does not return any value.
+ * 
+ * @function addLogoutListener
+ * 
+ * @returns {void} This function does not return any value. It performs side effects such as modifying `localStorage` and updating the webpage.
+ * 
+ */
+
 function addLogoutListener() {
   localStorage.removeItem("currentUser");
   renderLandingPage();
