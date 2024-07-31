958c1a20ca9bc907f237f1d6db6cab45f1c80043
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-08-01 01:08:44 +0330

    docs(pages): buy book list page Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- pages/buyBookListPage/renderBuyBookListPage.js
+++ pages/buyBookListPage/renderBuyBookListPage.js
@@ -1,10 +1,26 @@
+import calculateAndDisplayTotalPrice from "../../src/js/buyBooks/getTotalPrice";
 import getBoughtList from "../../src/js/buyBooks/getBougthList";
 import buyBookListPage from "./buyBookListPage";
+
+/**
+ * Renders the buy book list page and populates it with purchased book data.
+ * This function:
+ * 1. Clears the content of the `main` element.
+ * 2. Appends the generated book list page to the `main` element.
+ * 3. Fetches and displays the list of bought books using `getBoughtList`.
+ *
+ * @function renderBuyBookListPage
+ * @returns {void} - This function does not return a value.
+ */
 
 function renderBuyBookListPage() {
   let main = document.getElementById("main");
   main.innerHTML = "";
   main.append(buyBookListPage());
   getBoughtList();
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  if (currentUser) {
+    calculateAndDisplayTotalPrice(currentUser.name);
+  }
 }
 export default renderBuyBookListPage;
