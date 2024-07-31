ce015a54640790e79ccf9e14bdd71fe4d572d651
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:25:56 +0330

    docs(pages): admin page Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- pages/adminPage/renderTables.js
+++ pages/adminPage/renderTables.js
@@ -1,6 +1,14 @@
 import fetchBooksAndRenderTable from "../../components/booksTableComponent/booksTableComponent";
 import fetchUsersAndRenderTable from "../../components/UsertableComponent/tableComponent";
 
+/**
+ * Asynchronously fetches and renders the user and book tables.
+ * This function calls fetchUsersAndRenderTable and fetchBooksAndRenderTable to fetch data and render the respective tables.
+ * It doesn't take any parameters and doesn't return any value.
+ * Both functions are assumed to handle their own data fetching and rendering logic.
+ * @async
+ * @function renderTables
+ */
 async function renderTables() {
   fetchUsersAndRenderTable();
   fetchBooksAndRenderTable();
