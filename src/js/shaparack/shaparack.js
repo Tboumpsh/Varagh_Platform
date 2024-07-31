8b749c002ad480e8f007b97db08ee872dd7d3268
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:20:18 +0330

    Refactor(src):shaparack Calculate the price based on the number of weeks and months and then the user's connection to the port.

--- src/js/shaparack/shaparack.js
+++ src/js/shaparack/shaparack.js
@@ -2,6 +2,19 @@ import "/Lib/silverBox/silverBox.min.scss";
 import axios from "axios";
 
 import silverBox from "/Lib/silverBox/silverBox.min";
+import initiatePayment from "./pay";
+
+/**
+ * Handles the purchase of a membership plan by prompting the user for the duration, calculating the final price, and updating the user data.
+ * If the user is not logged in, shows an alert to log in first.
+ *
+ * @async
+ * @function handlePurchase
+ * @param {string} planType - The type of the membership plan to be purchased.
+ * @param {number} basePrice - The base price of the membership plan.
+ * @returns {Promise<void>} - A promise that resolves when the purchase is handled.
+ * @throws Will throw an error if there is an issue with the HTTP request to update the user data.
+ */
 
 async function handlePurchase(planType, basePrice) {
   const user = JSON.parse(localStorage.getItem("currentUser"));
@@ -45,6 +58,9 @@ async function handlePurchase(planType, basePrice) {
     await axios.patch(`http://localhost:3000/user/${user.id}`, {
       membershipPlan,
     });
+
+    // آغاز فرآیند پرداخت بعد از موفقیت در بروزرسانی اطلاعات کاربر
+    await initiatePayment(finalPrice);
   } catch (error) {
     silverBox({
       alertIcon: "error",
