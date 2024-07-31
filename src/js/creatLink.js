5f84aa793886cd97a15be74fe33dd1ccecb9e8dc
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:34:02 +0330

    docs(src): create link Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/js/creatLink.js
+++ src/js/creatLink.js
@@ -1,3 +1,30 @@
+/**
+ * Creates an array of `<a>` HTML elements with specified texts.
+ * 
+ * This function generates a specified number of `<a>` HTML elements and assigns each one text from the given
+ * `texts` array. The number of elements created is determined by the `number` parameter. If the `texts` array
+ * does not have enough elements to match the specified number, an error is logged to the console and the function
+ * returns `undefined`.
+ * 
+ * **Error Handling:**
+ * - If the length of the `texts` array is less than the `number` parameter, an error message is logged to the console.
+ * 
+ * **Return Value:**
+ * - An array of `<a>` HTML elements with the corresponding text content.
+ * 
+ * **Example:**
+ * ```javascript
+ * // Create 3 links with specified texts
+ * const links = createLinks(3, ["Home", "About", "Contact"]);
+ * console.log(links); // Array of <a> elements with text "Home", "About", and "Contact"
+ * ```
+ * 
+ * @param {number} number - The number of `<a>` elements to create.
+ * @param {string[]} texts - An array of strings to use as the text content for the `<a>` elements. 
+ * @returns {HTMLElement[]} An array of `<a>` HTML elements with the text content from the `texts` array.
+ * @throws {Error} If `texts` array length is less than `number`, an error is logged to the console.
+ */
+
 function createLinks(number, texts) {
   // Create an array to store the tooltips
   if (texts.length < number) {
