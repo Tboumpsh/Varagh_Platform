63fa4c2b8133a94c767522a45b581f7e4f668b6c
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:36:30 +0330

    docs(src): check user information Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/js/RegisterFormValidation/validationForm.js
+++ src/js/RegisterFormValidation/validationForm.js
@@ -5,6 +5,33 @@ import renderLandingPage from "../../../pages/landingPage/renderLandingPage";
 import renderAdminPage from "../../../pages/adminPage/renderingAdminPage";
 import silverBox from "/Lib/silverBox/silverBox.min";
 
+/**
+ * Sets up event listeners for the login and register buttons on a registration form.
+ * 
+ * This function adds event listeners to the login and register buttons. When the login button is clicked,
+ * it checks if the credentials match a hardcoded admin user or if they match any user in the database. 
+ * If the credentials are correct, it stores the user information in `localStorage` and redirects to the appropriate page.
+ * When the register button is clicked, it checks if the username is already registered. If not, it registers
+ * the new user and displays a confirmation message.
+ * 
+ * **Login Behavior:**
+ * - If the username is "support" and the password is "vara_1382", the user is considered an admin.
+ * - Admin users are redirected to the admin page.
+ * - Regular users are redirected to the landing page after successful login.
+ * - Displays an error message if the credentials are incorrect.
+ * 
+ * **Registration Behavior:**
+ * - Checks if the username is already registered.
+ * - Registers the new user if the username is not taken and displays a success message.
+ * - Displays an information message if the username is already registered.
+ * 
+ * @function checkUserInformation
+ * 
+ * @returns {void} This function does not return a value. It performs side effects such as adding event listeners
+ * and interacting with the DOM and external services.
+ * 
+ */
+
 function checkUserInformation() {
   const logInButton = document.getElementById("logInButton");
   const registerButton = document.getElementById("registerButton");
