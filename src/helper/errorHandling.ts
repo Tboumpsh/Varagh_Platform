9fa24db24ad8b37c1819c65229324e1c4b00a98b
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:36:11 +0330

    docs(src): error handelling Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/helper/errorHandling.ts
+++ src/helper/errorHandling.ts
@@ -3,6 +3,30 @@ type ErrorDetails = {
   stack?: string;
 };
 
+/**
+ * @typedef {Object} ErrorDetails
+ * @property {string} message - A descriptive message about the error.
+ * @property {string} [stack] - An optional stack trace of the error, providing additional details about the error's origin.
+ */
+
+/**
+ * Logs error details to the console.
+ * 
+ * This function prints the error message and, if available, the stack trace to the console. It is used to provide 
+ * detailed information about an error for debugging purposes.
+ * 
+ * @param {ErrorDetails} error - An object containing the error message and optionally the stack trace.
+ * @returns {void}
+ * 
+ * @example
+ * // Example usage:
+ * logError({
+ *   message: 'An unexpected error occurred',
+ *   stack: 'Error at function XYZ in file abc.js: line 23'
+ * });
+ */
+
+
 // A function to log error details
 function logError(error: ErrorDetails): void {
   console.error(`Error: ${error.message}`);
