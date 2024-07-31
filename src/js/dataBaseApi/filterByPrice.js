34becfc5152d45e49ee43cdb38bc64486149d153
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:44:42 +0330

    docs(src):filter by price Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/js/dataBaseApi/filterByPrice.js
+++ src/js/dataBaseApi/filterByPrice.js
@@ -2,6 +2,30 @@ import axios from "axios";
 
 import cardGenerator from "../../../components/cardComponent/cardComponent";
 
+/**
+ * Fetches a list of books from the server, sorts them by price, and displays them.
+ * 
+ * This asynchronous function retrieves books from the server and sorts them based on the provided order.
+ * The books are sorted by price in either ascending or descending order depending on the `order` parameter.
+ * After sorting, card elements are generated for each book and appended to the element with ID `productsContent`.
+ * 
+ * The sorting order is determined by the `order` parameter:
+ * - If `order` is `'asc'`, the books are sorted in ascending order (cheapest first).
+ * - If `order` is `'desc'`, the books are sorted in descending order (most expensive first).
+ * 
+ * If there is an error during the fetch operation, the error is logged to the console.
+ * 
+ * @async
+ * @function filterByPrice
+ * @param {string} order - The order in which to sort the books by price. Can be `'asc'` for ascending or `'desc'` for descending.
+ * @returns {Promise<void>} A promise that resolves when the books have been fetched, sorted, and displayed.
+ * 
+ * @throws {Error} Throws an error if the GET request fails or if there is an issue with
+ *     processing the response data.
+ * 
+ */
+
+
 async function filterByPrice(order) {
   let productsContent = document.getElementById('productsContent')
   try {
