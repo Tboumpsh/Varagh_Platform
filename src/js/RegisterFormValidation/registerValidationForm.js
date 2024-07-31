98eb420f15bef64d6a632a326615d9f69517e823
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:34:51 +0330

    docs(src): show register form Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/js/RegisterFormValidation/registerValidationForm.js
+++ src/js/RegisterFormValidation/registerValidationForm.js
@@ -1,8 +1,30 @@
 import "/Lib/silverBox/silverBox.min.scss";
-import axios from "axios";
 
 import silverBox from "/Lib/silverBox/silverBox.min";
 import checkUserInformation from "./validationForm";
+
+/**
+ * Displays a registration form using the `silverBox` library and sets up user validation.
+ * 
+ * This function uses `silverBox` to show a modal dialog with a custom registration form.
+ * The form includes input fields for username and password, and buttons for logging in and registering.
+ * After displaying the form, it calls `checkUserInformation()` to set up validation for the input fields.
+ * 
+ * The registration form includes the following components:
+ * - **Custom Icon**: A custom icon is displayed at the top of the modal.
+ * - **Title**: The main title of the modal.
+ * - **Text**: Instructions for the user to enter their information.
+ * - **Footer**: A footer text displayed at the bottom of the modal.
+ * - **Confirm Button**: A button labeled "ورود" (Login), which does not close the modal on click.
+ * - **Custom Button**: A button labeled "ثبت نام" (Register), which does not close the modal on click.
+ * - **Input Fields**: Includes fields for username and password with respective labels, placeholders, and hints.
+ * 
+ * @async
+ * @function showRegisterForm
+ * 
+ * @returns {void} This function does not return any value. It performs side effects such as displaying a modal and setting up validation.
+ */
+
 
 async function showRegisterForm() {
   silverBox({
