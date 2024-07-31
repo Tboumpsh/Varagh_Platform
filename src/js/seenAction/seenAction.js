442fadabd265b126f49e3a146c9b418d5ba7d20d
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:35:06 +0330

    docs(src): seen action Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- src/js/seenAction/seenAction.js
+++ src/js/seenAction/seenAction.js
@@ -2,6 +2,36 @@ import "/Lib/silverBox/silverBox.min.scss";
 
 import silverBox from "/Lib/silverBox/silverBox.min";
 
+/**
+ * Displays a notification using silverBox and plays an audio file a specified number of times.
+ * 
+ * This function uses the `silverBox` library to show a notification with a custom message and icon. It also 
+ * plays a specified audio file a certain number of times. The notification will be displayed for 5 seconds.
+ * 
+ * **Notification Details:**
+ * - **Icon:** Custom GIF icon (`/public/images/mobile-girl-mim.gif`).
+ * - **Title Text:** A message expressing the designers' reluctance to track user interactions on the page.
+ * - **Duration:** The notification will be displayed for 5 seconds.
+ * 
+ * **Audio Playback:**
+ * - The audio file (`/public/music/khandeh.mp3`) will be played repeatedly up to the specified count.
+ * - The playback is managed by a counter (`playCount`) to track the number of times the audio has been played.
+ * 
+ * **Event Handling:**
+ * - An `ended` event listener is used to increment the play count and trigger the next playback if the count 
+ *   limit has not been reached.
+ * 
+ * **Error Handling:**
+ * - If there is an error while playing the audio, it is logged to the console.
+ * 
+ * @function seenAction
+ * @param {number} count - The number of times the audio should be played.
+ * 
+ * @returns {void} This function does not return a value. It performs side effects such as displaying a notification
+ * and playing audio.
+ */
+
+
 function seenAction(count) {
   silverBox({
     timer: 5000,
