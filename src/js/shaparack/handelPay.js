c9cc712eaaaac6973525ba53c2e12c2e5d44c286
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:11:17 +0330

    Feat(src):handle pay Sink of the price and the payment gateway to render it to the payment page.

---
+++ src/js/shaparack/handelPay.js
@@ -0,0 +1,24 @@
+import calculateTotalPrice from "./calculateTotal";
+import initiatePayment from "./pay";
+
+/**
+ * Handles the payment process by calculating the total price and initiating the payment.
+ *
+ * @param {string} userName - The name of the user whose bought list is to be processed.
+ * @returns {Promise<void>} - A promise that resolves when the payment process has been completed.
+ */
+async function handlePayment(userName) {
+  try {
+    const totalPrice = await calculateTotalPrice(userName);
+    if (totalPrice > 0) {
+      await initiatePayment(totalPrice);
+    } else {
+      alert("سبد خرید شما خالی است.");
+    }
+  } catch (error) {
+    console.error("Error handling payment:", error);
+    alert('خطایی در فرآیند پرداخت رخ داده است.');
+  }
+}
+
+export default handlePayment;
