a3932c4169c84c2ae717ca68e9eb8148a1b5c4ae
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:45:11 +0330

    docs(component):divider components Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- components/dividerComponent/dividerComponent.js
+++ components/dividerComponent/dividerComponent.js
@@ -1,6 +1,22 @@
 import domGenerator from "dom-generator";
 import "./index.scss";
 
+/**
+ * Generates a divider component with optional images on the left and right sides.
+ * 
+ * This function creates a divider component that displays two images: one on the left and one on the right.
+ * The images are specified by the `imageL` and `imageR` parameters. If no image is provided for one side, 
+ * that side will be empty.
+ * 
+ * @function dividerComponent
+ * @param {Object} options - Configuration object for the divider component.
+ * @param {string} [options.imageR=""] - The URL of the image to display on the right side of the divider. Defaults to an empty string.
+ * @param {string} [options.imageL=""] - The URL of the image to display on the left side of the divider. Defaults to an empty string.
+ * 
+ * @returns {HTMLElement} The generated divider component element containing the images.
+ * 
+ */
+
 function dividerComponent({ imageR = "", imageL = "" }) {
   let divider = domGenerator({
     tag: "div",
