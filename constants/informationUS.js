25249be5fa1d87ee92977dedb4c79c2130e5f2fd
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-07-31 23:23:30 +0330

    docs(constant): constant variable Add full description of the function of the function and the inputs it takes. Specifying each input and a brief explanation of the function of the function in standard and international mode.

--- constants/informationUS.js
+++ constants/informationUS.js
@@ -1,7 +1,28 @@
 export const Telephone = 2154658;
+
+/**
+ * The mobile number of the contact.
+ * @type {number}
+ */
 export const Mobile = 2154658;
+/**
+ * The address of the contact.
+ * @type {string}
+ */
 export const Address = "تهرانـ و برای شرایط فعلی تکنولوژی مورد نیازوع با هاشدی";
+/**
+ * The weekly price for the service.
+ * @type {number}
+ */
 
 export const WeeklyPrice = 200000;
+/**
+ * The yearly price for the service.
+ * @type {number}
+ */
 export const yearlyPrice = 75000000;
+/**
+ * The monthly price for the service.
+ * @type {number}
+ */
 export const monthlyPrice = 7000000;
