d38a7e65bed301ca7b9c19eea6bc69fc88fecaad
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-08-05 19:45:44 +0330

    Fixed(src): header component effect Changing the animation style while scrolling and soft return to the initial state when returning to the first page.

--- src/js/headerEffect/headerEffect.js
+++ src/js/headerEffect/headerEffect.js
@@ -8,6 +8,8 @@ function headerEffect(header) {
         header.style.height = "110px";
         header.style.width = "100%";
         header.style.top = "0";
+        header.style.borderRadius = "0";
+        header.style.transition = "all 0.2s"
         isAtTop = false;
   
         // Clear any existing timer
@@ -16,7 +18,8 @@ function headerEffect(header) {
         // Scroll back to top behavior
         header.style.height = ""; // Reset height
         header.style.width = ""; // Reset width
-        header.style.top = ""; // Reset top
+        header.style.top = "";
+        header.style.borderRadius = "15px" // Reset top
         isAtTop = true;
       }
     }
